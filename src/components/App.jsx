3844ccefc247b403bd1a7d32389163147810e00b
SEKCJ <caesar@ea-vr.org>
2020-03-05 13:47:02 -0600

    moved routes to directory Pages, changed imports on App.jsx file, made Home page main route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from "./Home";
 import Navbar from "./Navbar"
-import Week from "./Week";
-import Day from "./Day";
-import Meds from "./Meds";
-import SingleMeds from "./SingleMed";
-import Doctors from "./Doctors";
-import Pharmacies from "./Pharmacies"
+import Home from "./Pages/Home";
+import Week from "./Pages/Week";
+import Day from "./Pages/Day";
+import Meds from "./Pages/Meds";
+import SingleMeds from "./Pages/SingleMed";
+import Doctors from "./Pages/Doctors";
+import Pharmacies from "./Pages/Pharmacies"
 
 
 const App = () => {
@@ -16,15 +16,16 @@ const App = () => {
     <Router>
       <Navbar />
       <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/Week" component={Week} />
-          <Route exact path="/Day" component={Day} />
-          <Route exact path="/Meds" component={Meds} />
-          <Route exact path="/SingleMeds" component={SingleMeds} />
-          <Route exact path="/Doctors" component={Doctors} />
-          <Route exact path="/Pharmacies" component={Pharmacies} />
-        </Switch>
+        <Route exact path="/Week" component={Week} />
+        <Route exact path="/Day" component={Day} />
+        <Route exact path="/Meds" component={Meds} />
+        <Route exact path="/SingleMeds" component={SingleMeds} />
+        <Route exact path="/Doctors" component={Doctors} />
+        <Route exact path="/Pharmacies" component={Pharmacies} />
+        <Route path="/" component={Home} />
+      </Switch>
     </Router>
-  )};
+  )
+};
 
 export default App;
